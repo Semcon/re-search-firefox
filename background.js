cea4b6e63b9f16fb164b6b7aa8396c080aae15fe
Basim Ali <s23461@semcon.se>
2016-10-25 13:17:45 +0200

    reorganized files

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-
-
-var val ="";
+var val = "";
 var jsonTermsGoogle;
 var jsonTermsBing;
 var googleURL;
@@ -8,6 +6,7 @@ var bingURL;
 var currentTerms;
 var currentURL;
 var jsonData;
+var doLog = false;
 
 
 console.log('background is running');
@@ -17,7 +16,7 @@ console.log('background is running');
 
 val = localStorage.getItem("runState");
 console.log('val: ', val);
-if(val === null){
+if (val === null) {
   localStorage.setItem('runState', 'enabled')
   console.log('Saved', 'runState', 'enabled');
   val = 'enabled';
@@ -25,152 +24,209 @@ if(val === null){
 
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.myjson.com/bins/4we1m", true);
+xhr.open("GET", 'https://api.myjson.com/bins/4e30w', true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState === 4 && xhr.status === 200) {
     jsonData = JSON.parse(xhr.responseText);
   }
 }
 xhr.send();
- 
 
-function showWindows(request){
-  if(typeof currentURL !== 'undefined' && typeof currentTerms !== 'undefined'){
+
+function showWindows(request) {
+  if (typeof currentURL !== 'undefined' && typeof currentTerms !== 'undefined') {
     var link = currentURL + currentTerms[request];
-    console.log('Link: ' , link);
-    chrome.windows.getCurrent( {}, function( window ){
-        console.log( window );
-         saveWindowInfo(window);
-         chrome.windows.create( {
-          height: parseInt(window.height),
-          left: parseInt(window.width / 2 + 8),
-          state: 'normal',
-          top: parseInt(0),
-          type: 'normal',
-          url: link,
-          width: parseInt(window.width / 2 + 8)
-        });
+    console.log('Link: ', link);
+    chrome.windows.getCurrent({}, function(window) {
+      console.log(window);
+      saveWindowInfo(window);
+      chrome.windows.create({
+        height: parseInt(window.height),
+        left: parseInt(window.width / 2 + 8),
+        state: 'normal',
+        top: parseInt(0),
+        type: 'normal',
+        url: link,
+        width: parseInt(window.width / 2 + 8)
+      });
 
-        chrome.windows.update( window.id, {
-          height: parseInt(window.height),
-          state: 'normal',
-          width: parseInt(window.width / 2 + 8)
-        });
+      chrome.windows.update(window.id, {
+        height: parseInt(window.height),
+        state: 'normal',
+        width: parseInt(window.width / 2 + 8)
+      });
     });
-  }
-  else{
+  } else {
     console.log('currentURL and/or currentTerms is undefined');
   }
 }
 
-function saveWindowInfo(window){
+function saveWindowInfo(window) {
   //store old window size
   console.log("1");
-      var mainWindowWidth                = window.width;
-      var mainWindowHeight               = window.height;
-      var windowID                       = window.id;
-      var mainWindowDict                 = {};
-      mainWindowDict["mainWindowHeight"] = mainWindowHeight;
-      mainWindowDict["mainWindowWidth"]  = mainWindowWidth;
-      mainWindowDict["windowID"]        = windowID;
-      localStorage.setItem('mainWindowDict', JSON.stringify(mainWindowDict)); 
-      console.log(localStorage.getItem("mainWindowDict"));
+  var mainWindowWidth = window.width;
+  var mainWindowHeight = window.height;
+  var windowID = window.id;
+  var mainWindowDict = {};
+  mainWindowDict["mainWindowHeight"] = mainWindowHeight;
+  mainWindowDict["mainWindowWidth"] = mainWindowWidth;
+  mainWindowDict["windowID"] = windowID;
+  localStorage.setItem('mainWindowDict', JSON.stringify(mainWindowDict));
+  console.log(localStorage.getItem("mainWindowDict"));
 }
+
+
+
+function getSelector(request, sender, sendResponse) {
+  //content script is asking for selector
+  var url = request.url;
+  // Loop over all engines
+  if (typeof jsonData !== 'undefined' && typeof url !== 'undefined') {
+    for (var i = 0; i < jsonData.engines.length; i = i + 1) {
+      var matchCount = 0;
+
+      // Loop over all required matches for the engine
+      for (var matchIndex = 0; matchIndex < jsonData.engines[i].match.length; matchIndex = matchIndex + 1) {
+        if (url.indexOf(jsonData.engines[i].match[matchIndex]) > -1) {
+          // We have a match, increment our counter
+          matchCount = matchCount + 1;
+          if (doLog) {
+            console.log('found match, matchCount: ', matchCount);
+          }
+        }
+      }
+
+      // If we have the same number of matches as required matches we have a valid site
+      if (matchCount === jsonData.engines[i].match.length) {
+        if (doLog) {
+          console.log('Valid site');
+        }
+
+        var engineTerms = jsonData.engines[i].terms;
+        var currentLanguage = jsonData.engines[i].language;
+        currentTerms = jsonData.terms[engineTerms][currentLanguage];
+        currentURL = jsonData.engines[i].url;
+        var selectorInput = jsonData.engines[i].selectors.input;
+        var selectorBtn;
+        var selectorAutoCmpl;
+
+        if (jsonData.engines[i].selectors.hasOwnProperty('button')) {
+          selectorBtn = jsonData.engines[i].selectors.button;
+        }
+
+        if (jsonData.engines[i].selectors.hasOwnProperty('autocomplete')) {
+          selectorAutoCmpl = jsonData.engines[i].selectors.autocomplete;
+        }
+
+        sendResponse({
+          selectorSearchField: selectorInput,
+          selectorButton: selectorBtn,
+          selectorAutoComplete: selectorAutoCmpl,
+          terms: currentTerms
+        });
+
+        return true;
+      }
+    }
+
+    if (doLog) {
+      console.log('If not valid site, Url:', url);
+    }
+
+    sendResponse({
+      selectorSearchField: false
+    });
+  }
+}
+
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     //content script asks if extension is on/off
-    if (request.runState === '?'){
-     
+    if (request.runState === '?') {
+
       console.log(localStorage.getItem("runState"))
       var value = localStorage.getItem("runState")
-      sendResponse({"runState": value})
-
+      sendResponse({
+        "runState": value
+      })
     }
 
     //content script is asking for selector
-    else if(request.selector === 'selector'){
-      var url = request.url;
-      // Loop over all engines
-      if(typeof jsonData !== 'undefined' && typeof url !== 'undefined'){
-        for( var i = 0; i < jsonData.engines.length; i = i + 1 ){
-          var matchCount = 0;
-
-          // Loop over all required matches for the engine
-          for( var matchIndex = 0; matchIndex < jsonData.engines[ i ].match.length; matchIndex = matchIndex + 1 ){
-            if( url.indexOf( jsonData.engines[ i ].match[ matchIndex ] ) > -1 ){
-              // We have a match, increment our counter
-              matchCount = matchCount + 1;
-              console.log('found match, matchCount: ', matchCount);
-            }
-          }
-
-          // If we have the same number of matches as required matches we have a valid site
-          if( matchCount === jsonData.engines[ i ].match.length ){
-            console.log( 'Valid site' );
-            currentTerms = jsonData.engines[i].terms;
-            currentURL = jsonData.engines[i].url;
-            sendResponse({selector: jsonData.engines[i].selector});
-            return true;
-          }
-        }
-        console.log('If not valid site, Url:' , url);
-        sendResponse({selector: false});
-      }
+    else if (request.selector === 'selector') {
+      getSelector(request, sender, sendResponse);
     }
 
+
+
     //content script is sending terms
-    else if(typeof currentTerms !== 'undefined' && currentTerms.hasOwnProperty(request)){
+    else if (typeof currentTerms !== 'undefined' && currentTerms.hasOwnProperty(request)) {
       console.log('term is found');
-      sendResponse({status: 'term was found'});
+      sendResponse({
+        status: 'term was found'
+      });
       showWindows(request);
     }
 
     //From popup
-    else if(request.runState === 'changeState'){
-      console.log('ChangeState from popup / current value is: ' , val);
-      if(val  === 'enabled'){
-        localStorage.setItem('runState', 'disabled'); 
-          console.log('Saved', 'runState', 'disabled');
-          val = 'disabled';
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {runState: "disabled"}, function(response) {
-              if(response){
-                console.log(response.message);
-              }
-              else{
-                console.log('Content script not injected');
-              }
-            });
+    else if (request.runState === 'changeState') {
+      console.log('ChangeState from popup / current value is: ', val);
+      if (val === 'enabled') {
+        localStorage.setItem('runState', 'disabled');
+        console.log('Saved', 'runState', 'disabled');
+        val = 'disabled';
+        chrome.tabs.query({
+          active: true,
+          currentWindow: true
+        }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            runState: "disabled"
+          }, function(response) {
+            if (response) {
+              console.log(response.message);
+            } else {
+              console.log(response.message);
+              console.log('Content script not injected');
+            }
           });
-          sendResponse({runState: val});
-        
-      }
-      else if(val === 'disabled'){
-        localStorage.setItem('runState', 'enabled') 
-          console.log('Saved', 'runState', 'enabled');
-          val = 'enabled';
-          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {runState: "enabled"}, function(response) {
-              if(response){
-                console.log(response.message);
-                console.log("Content script injected");
-              }
-              else{
-                console.log('Content script not injected');
-              }
-            });
+        });
+        sendResponse({
+          runState: val
+        });
+
+      } else if (val === 'disabled') {
+        localStorage.setItem('runState', 'enabled')
+        console.log('Saved', 'runState', 'enabled');
+        val = 'enabled';
+        chrome.tabs.query({
+          active: true,
+          currentWindow: true
+        }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            runState: "enabled"
+          }, function(response) {
+            if (response) {
+              console.log(response.message);
+              console.log("Content script injected");
+            } else {
+              console.log('Content script not injected');
+            }
           });
-          sendResponse({runState: val});
+        });
+        sendResponse({
+          runState: val
+        });
       }
-    }
-    else if(request.runState === 'getState'){       
-      sendResponse({runState: val});     
+    } else if (request.runState === 'getState') {
+      sendResponse({
+        runState: val
+      });
       console.log('Message to event page was: ', request);
-    }
-    else{
+      console.log("request.runstate is: ", request.runState);
+    } else {
       console.log('Message to event page was not handled: ', request);
+      console.log("request.runstate is: ", request.runState);
     }
     return true;
-});
+  });
--- content.js
+++ content.js
@@ -1,6 +1,8 @@
+(function(){
 var runState;
 var runInit = true;
 var elements;
+var runSetUI = true;
 //var tabs = require('sdk/tabs');
 
 function sendText( text ){
@@ -14,9 +16,9 @@ function sendText( text ){
   }
 }
 
-function getSearchTerm(selector){
- console.log('Selector: ', selector);
- elements = document.querySelectorAll(selector);
+function getSearchTerm(selectorInput, selectorButton, selectorAutoComplete){
+ console.log('Selector: ', selectorInput);
+ elements = document.querySelectorAll(selectorInput);
  if( elements.length === 0 ){
    setTimeout( init, 100 );
    return false;
@@ -26,33 +28,85 @@ function getSearchTerm(selector){
  if( element.value.length > 0 ){
     sendText( element.value );
  }
-/*  element.addEventListener( 'input', function( event ){
-    sendText( event.target.value );
- });*/
+if(typeof selectorAutoComplete !== 'undefined'){
+            window.addEventListener( 'keydown', function( event ){
+                if( event.keyCode === 13 ){
+                    console.log('enter was pressed');
+                    sendText(element.value);
+                }
+            });
 
- element.addEventListener("keyup", function(event) {
-     console.log('went in to eventlistener');
-     event.preventDefault();
-     if (event.keyCode == 13) {
-         alert('it works!');
-     }
- });
+            window.addEventListener('click', function (event) {
+                console.log(event);
+                if( String(event.target.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
+                    console.log('autocomplete was clicked');
+                    sendText(event.target.outerText);
+                } else if(String(event.target.parentElement.classList).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1 ){
+                    console.log('autocomplete was clicked');
+                    sendText(event.target.parentElement.outerText);
+                } else if(event.target.children.length > 0){
+                    for( var i = 0; i < event.target.children.length; i++ ){
+                        if(event.target.children[i].children.length > 0){
+                            for( var j = 0; j < event.target.children[i].children.length; j++ ){
+                                if(String(event.target.children[i].children[j].className).indexOf( selectorAutoComplete.replace( '.', '' ) ) > -1){
+                                    console.log('autocomplete was clicked');
+                                    sendText( event.target.children[i].children[j].outerText );
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        }
 
- document.querySelectorAll('.lsb')[0].addEventListener('click', function (e) {
-     console.log('went into function');
-     sendText(element.value);
-     var key = e.which || e.keyCode;
-     if (key === 13) { // 13 is enter
-       alert('enter or search button was pressed');
-     }
- });
-}
+        if(typeof selectorButton !== 'undefined'){
+            document.querySelectorAll(selectorButton)[0].addEventListener('click', function () {
+                console.log('search button was clicked');
+                sendText(element.value);
+            });
+        }
+    }
+
+function setUI(selectorSearchField, englishTerms){
+  console.log("ASdasdasdasdasd");
+      if(selectorSearchField === '.gsfi'){
+
+        //Adapt Google's UI
+        document.querySelectorAll('.sfbgg')[0].setAttribute("style","height: 90px");
+        document.getElementById('top_nav').setAttribute("style","margin-top: 31px;");
+
+        //Create and append select list
+        var selectList = document.createElement("SELECT");
+        selectList.setAttribute("style","height: 25px; width: 160px; margin-top: 5px");
+        selectList.id = "termList";
+        document.querySelectorAll('.tsf-p')[0].appendChild(selectList);
+        var defaultOption = document.createElement("option");
+        defaultOption.value = 'Other Re-search terms';
+        defaultOption.text = 'Other Re-search terms';
+        selectList.add(defaultOption);
+      }
+
+      //Create and append the options
+      for (var i = 0; i < Object.keys(englishTerms).length; i++) {
+          var option = document.createElement("option");
+          option.value = Object.keys(englishTerms)[i];
+          option.text = Object.keys(englishTerms)[i];
+          selectList.add(option);
+      }
+    }
+
 
 function init(){
   console.log('In init');
   chrome.runtime.sendMessage({selector: "selector", url: window.location.href}, function(response) {
-    if(response.selector !== false){
-      getSearchTerm(response.selector);
+    console.log(response);
+    if(response.selectorSearchField !== false){
+      if(runSetUI !== false){
+                  setUI(response.selectorSearchField, response.terms);
+                  runSetUI = false;
+                }
+                getSearchTerm(response.selectorSearchField, response.selectorButton, response.selectorAutoComplete);
+      getSearchTerm(response.selectorSearchField);
     }
     else{
       console.log('Selector not found');
@@ -60,12 +114,21 @@ function init(){
   });
 }
 
+function runWhenReady(){
+    if( document.readyState !== 'complete' ){
+        console.log("running")
+        setTimeout( runWhenReady, 100 );
+        return false;
+}
 
 //first time content script runs
 if( document.readyState === 'complete' ){
   console.log('document is complete');
+
   console.log('Run init: ', runInit);
-  chrome.runtime.sendMessage({runState: "?"}, function(response) {
+  chrome.runtime.sendMessage({
+    runState: "?"
+  }, function(response) {
     runState = response.runState;
     console.log('runState in contentscript: ', runState);
 
@@ -80,32 +143,35 @@ if( document.readyState === 'complete' ){
   });
 }
 
-
-
-
+//first time content script runs
+console.log("before running")
+runWhenReady();
+console.log("after running")
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.runState === 'disabled'){
-      runState = request.runState;
-      console.log('runstate: ', runState);
-      sendResponse({message: 'received disabled'});
-    }
-    else if(request.runState === 'enabled'){
-      runState = request.runState;
-      console.log('runstate: ', runState);
+        function( request, sender, sendResponse ) {
+            if ( request.runState === 'disabled' ){
+                runState = request.runState;
 
-      if( document.readyState === 'complete' ){
-          console.log('document is complete');
-          if(runInit === true){
-              init();
-              runInit = false;
-          }
-      }
-      sendResponse({message: 'received enabled'});
-    }
-    else{
-      console.log('Message from event page was not handled');
-    }
-    return true;
-});
+                sendResponse({
+                    message: 'received disabled'
+                });
+            } else if( request.runState === 'enabled' ){
+                runState = request.runState;
+                if( document.readyState === 'complete' ){
+                    console.log('document is complete');
+                    if( runInit === true ){
+                        init();
+                        runInit = false;
+                    }
+                }
+                sendResponse({
+                    message: 'received enabled'
+                });
+            } else {
+                console.log( 'Message from event page was not handled' );
+            }
+            return true;
+        }
+    );
+}})();
